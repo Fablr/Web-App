ef3b853986b34a08f50821184d571461f55d8906
Dustin Replogle <dreplogle@Dustins-MacBook-Pro.local>
2015-06-29 18:56:01 -0700

    Added a bunch of files for AWS deployment. Had to add a www/static so apache would have place to read static files from.

--- fablersite/settings.py
+++ fablersite/settings.py
@@ -79,18 +79,32 @@ WSGI_APPLICATION = 'fablersite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'fabler',
-        'USER': 'fabler@weopxlv27f',
-        'PASSWORD': '030821ae!',
-        'OPTIONS': {
-            'host_is_server': True,
-            'dsn': 'fabler'  # as in odbc.ini
-        },
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ebdb',
+        'USER': 'dreplogle',
+        'PASSWORD': '030821ae!',
+        'HOST': 'aa1rr2xlfemsxmo.cp4q3xdsxtdz.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
+        #'OPTIONS': {
+        #    'host_is_server': True,
+        #    'dsn': 'fabler'  # as in odbc.ini
+        #    },
+        }
+    }
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
@@ -124,8 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    #os.path.join(BASE_DIR, "static"),
 )
