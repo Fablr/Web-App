5a322610f8e281fb79c09b77355c5fb1029ae529
Chris Day <cday@fabler.fm>
2015-12-11 22:51:35 -0800

    Allow Users to be filterable by id.

--- fablersite/authentication/views.py
+++ fablersite/authentication/views.py
@@ -67,6 +67,7 @@ class RegistrationView(CreateView):
 class UserFilter(django_filters.FilterSet):
     class Meta:
         model = User
+        fields = ['id']
 
 # Django Rest API
 # ViewSets define the view behavior.
