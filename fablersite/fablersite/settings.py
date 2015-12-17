bf1b2c904817abbc94b100d594398934969e7cad
Dustin Replogle <dustin.replogle@fabler.fm>
2015-12-16 21:33:39 -0800

    Added logging for django production

--- fablersite/fablersite/settings.py
+++ fablersite/fablersite/settings.py
@@ -232,6 +232,65 @@ LOGGING['filters']['suppress_deprecated'] = {
 }
 LOGGING['handlers']['console']['filters'].append('suppress_deprecated')
 
+DEBUG_LOG_DIR = '/var/log/app/django_debug.log'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    # How to format the output
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    # Log handlers (where to go)
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'log_file': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': DEBUG_LOG_DIR,
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    # Loggers (where does the log come from)
+    'loggers': {
+        'repackager': {
+            'handlers': ['console', 'log_file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console', 'log_file'],
+            'level': 'WARN',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['console', 'log_file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 class SuppressDeprecated(logging.Filter):
     def filter(self, record):
         WARNINGS_TO_SUPPRESS = [
